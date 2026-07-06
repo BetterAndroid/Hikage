db883ac1c4a57c6a4baddf61825412c7880de05c
fankesyooni <qzmmcn@163.com>
2026-07-06 18:25:01 +0800

    chore: update configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,7 @@ pluginManagement {
         gradlePluginPortal()
         google()
         mavenCentral()
+        maven("https://raw.githubusercontent.com/HighCapable/maven-repository/main/repository/releases")
         mavenLocal()
     }
 }
