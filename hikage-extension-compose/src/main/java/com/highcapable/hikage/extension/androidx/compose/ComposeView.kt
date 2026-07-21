269ad99fb3a1221e30d23223cfc123dfba59f781
fankesyooni <qzmmcn@163.com>
2026-07-21 22:02:21 +0800

    refactor: complete ComposeView initialization

--- hikage-extension-compose/src/main/java/com/highcapable/hikage/extension/androidx/compose/ComposeView.kt
+++ hikage-extension-compose/src/main/java/com/highcapable/hikage/extension/androidx/compose/ComposeView.kt
@@ -59,17 +59,15 @@ fun <LP : ViewGroup.LayoutParams> Hikage.Performer<LP>.ComposeView(
     attrs: HikageAttribute = {},
     init: HikageView<ComposeView> = {},
     content: (@Composable () -> Unit)? = null
+) = View(
+    viewClass = ComposeView::class,
+    factory = { context, attrs -> ComposeView(context, attrs) },
+    lparams = lparams,
+    id = id,
+    attrs = attrs
 ) {
-    View(
-        viewClass = ComposeView::class,
-        factory = { context, attrs -> ComposeView(context, attrs) },
-        lparams = lparams,
-        id = id,
-        attrs = attrs
-    ) {
-        setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnDetachedFromWindowOrReleasedFromPool)
+    setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnDetachedFromWindowOrReleasedFromPool)
 
-        init(this)
-        content?.let { setContent(it) }
-    }
+    init(this)
+    content?.let { setContent(it) }
 }
