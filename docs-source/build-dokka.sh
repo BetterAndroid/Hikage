8ec589f01a7d233759096a098f8ee04c8b1a7b05
fankesyooni <qzmmcn@163.com>
2026-07-27 10:29:03 +0800

    docs: update Dokka publishing script and deployment configuration

--- docs-source/build-dokka.sh
+++ docs-source/build-dokka.sh
@@ -1,2 +1,7 @@
 cd ..
-./gradlew :hikage-core:publishKDoc :hikage-extension:publishKDoc :hikage-extension-betterandroid:publishKDoc :hikage-extension-compose:publishKDoc
+./gradlew :hikage-core:publishKDoc \
+:hikage-extension:publishKDoc \
+:hikage-extension-betterandroid:publishKDoc \
+:hikage-extension-compose:publishKDoc \
+:hikage-runtime:publishKDoc \
+:hikage-runtime-attribute:publishKDoc
